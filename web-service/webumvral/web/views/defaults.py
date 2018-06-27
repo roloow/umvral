166b350b74313877feb11bbb1f6950982445771b
rCasanueva <rolacasanueva@gmail.com>
2018-06-27 15:06:31 -0400

    urls + default view

---
+++ web-service/webumvral/web/views/defaults.py
@@ -0,0 +1,30 @@
+# -*- coding: utf-8 -*-
+from django.shortcuts import render, redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+import urllib
+
+def get_base_context(request, *args, **kwargs):
+    base_context = {
+    }
+    return base_context
+
+
+def logout_method(request):
+    logout(request)
+    return redirect('web:home')
+
+def login_method(request):
+    context = get_base_context(request)
+    if (request.method == 'GET'):
+        if (request.user.is_authenticated):
+            return render(request, 'web/home.html', context)
+        return render(request, 'web/login.html', context)
+    if (request.method == 'POST'):
+        user = authenticate(request, username=request.POST['username'], password=request.POST['password'])
+        if user:
+            login(request, user)
+            return render(request, 'web/home.html', context)
+        context['error'] = True
+        return render(request, 'web/login.html', context)
--- web-service/webumvral/webumvral/urls.py
+++ web-service/webumvral/webumvral/urls.py
@@ -16,10 +16,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     # path('/', include(administration.urls)) TO BE CREATED
     path('api/', include('api.urls')),
     path('web/', include('web.urls')),
-] + static(setting.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
