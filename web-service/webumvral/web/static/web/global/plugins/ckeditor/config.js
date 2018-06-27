e6a3c0a945bc64ccadfa39e1030709f6c9d90f43
rCasanueva <rolacasanueva@gmail.com>
2018-06-27 03:11:03 -0400

    - urls (sin contenido) - static metronic - Templates (base wrapper/page)

---
+++ web-service/webumvral/web/static/web/global/plugins/ckeditor/config.js
@@ -0,0 +1,25 @@
+/**
+ * @license Copyright (c) 2003-2016, CKSource - Frederico Knabben. All rights reserved.
+ * For licensing, see LICENSE.md or http://ckeditor.com/license
+ */
+
+CKEDITOR.editorConfig = function( config ) {
+	config.toolbarGroups = [
+		{ name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+		{ name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+		{ name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+		{ name: 'editing', groups: [ 'spellchecker', 'selection', 'find', 'editing' ] },
+		'/',
+		{ name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+		{ name: 'colors', groups: [ 'colors' ] },
+		{ name: 'forms', groups: [ 'forms' ] },
+		{ name: 'links', groups: [ 'links' ] },
+		{ name: 'insert', groups: [ 'insert' ] },
+		{ name: 'styles', groups: [ 'styles' ] },
+		{ name: 'tools', groups: [ 'tools' ] },
+		{ name: 'others', groups: [ 'others' ] },
+		{ name: 'about', groups: [ 'about' ] }
+	];
+
+	config.removeButtons = 'Save,NewPage,Preview,Print,Templates,PasteFromWord,PasteText,HiddenField,ImageButton,Button,Select,Textarea,TextField,Radio,Checkbox,Form,RemoveFormat,CopyFormatting,CreateDiv,BidiRtl,BidiLtr,Anchor,Flash,PageBreak,Iframe,Language,ShowBlocks,Maximize,About';
+};
