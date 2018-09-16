2016f646f19231b4d89be2c79e8f70d8374ccfaa
Andrés Cifuentes <aecifuentesv@gmail.com>
2018-09-16 17:57:41 -0300

    CRUD de Cursos
    Se tiene las operaciones de C, R y D.  Todavia falta los updates, debido a que al actualizar, Django crea un modelo nuevo

--- web-service/webumvral/web/urls.py
+++ web-service/webumvral/web/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path, re_path
 from . import views
+from .views.course import CourseListJson
+from django.conf.urls import url, include
+from django.contrib.auth.decorators import login_required
 
 app_name="web"
 
@@ -8,4 +11,10 @@ urlpatterns = [
     re_path(r'logout/$', views.logout_method, name='logout'),
     re_path(r'login/$', views.login_method, name='login'),
     re_path(r'register/$', views.register, name='register'),
-]
+    re_path(r'course/list$', views.courses_list, name='course_list'),
+    re_path(r'course/edit/$', views.course_edit, name='course_edit'),
+    re_path(r'course/edit/(?P<course>\w+)$', views.course_edit, name='course_edit'),
+    re_path(r'course/delete/$', views.course_delete, name='course_delete'),
+    re_path(r'course/delete/(?P<course>\w+)$', views.course_delete, name='course_delete'),
+    url(r'^my/datatable/data/$', login_required(CourseListJson.as_view()), name='course_list_json')
+] 
