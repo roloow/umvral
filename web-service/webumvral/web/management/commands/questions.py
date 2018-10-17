f248d3630bf76840ca343af80413d65312d50527
Camilo Saldías <camilo.saldias@gmail.com>
2018-10-17 16:08:37 -0300

    Comando para agregar preguntas a BD

--- web-service/webumvral/web/management/commands/questions.py
+++ web-service/webumvral/web/management/commands/questions.py
@@ -1,7 +1,9 @@
+# -*- coding: utf-8 -*-
+
 from django.core.management.base import BaseCommand, CommandError
 from web.models import *
 from django.contrib.auth.models import User
-from random import choice
+import codecs
 
 class Command(BaseCommand):
     help = 'Fill the database with question for tests. The document to import must be named: questions.txt'
@@ -22,5 +24,26 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
 
         if (options['beta']):
-            #TODO: https://github.com/roloow/umvral/issues/118
             return 0
+
+        if (options['import']):
+            #Importamos un archivo de preguntas, llamado questions.csv
+            print("Importando preguntas...")
+            preguntas = open('questions.csv', encoding='utf-8')
+            if not preguntas:
+                print("Error: archivo questions.csv no encontrado")
+                return -1
+            for pregunta in preguntas:
+                pregunta = pregunta.strip().split(';')
+                #print(pregunta[0])
+                q = QuestionModel()
+                q.statement = pregunta[1]
+                q.optionA = pregunta[2]
+                q.optionB = pregunta[3]
+                q.optionC = pregunta[4]
+                q.optionD = pregunta[5]
+                q.correct = pregunta[6]
+                experiencia = ExperienceModel.objects.filter(name=pregunta[0])
+                #print(experiencia[0].pk)
+                q.experience = experiencia[0]
+                q.save()
