5f9f2a68b1f9998d452aa81d080057a40c63dbb3
Camilo Saldías <camilo.saldias@gmail.com>
2018-10-23 11:09:47 -0300

    Agregado paquete para poder usar la API en Ionic Serve

--- web-service/webumvral/webumvral/settings.py
+++ web-service/webumvral/webumvral/settings.py
@@ -40,12 +40,14 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'api',
     'web',
-    'tastypie'
+    'tastypie',
+    'corsheaders'
 ]
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
@@ -53,6 +55,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
+CORS_ORIGIN_ALLOW_ALL = True
 ROOT_URLCONF = 'webumvral.urls'
 
 TEMPLATES = [
